95649ccb2e54b17ea4f26c7ae86857ab6e59af15
JackWang <jack00770066@gmail.com>
2018-12-05 01:52:48 +0800

    程式修正

--- password.py
+++ password.py
@@ -3,13 +3,14 @@ typetimes = 0
 chance = 3
 while typetimes < 3:
 	text = input('請輸入密碼:')
-	if text != 'a123456':
+	if text != password:
 		chance = chance - 1
-		print('密碼錯誤!還有', chance, '次機會')
-		typetimes += 1
 		if chance == 0:
-			print('錯誤次數過多，我們將鎖住您的帳號')
+			print('你沒機會了，錯誤次數過多，我們將鎖住您的帳號')
 			break
-	elif password == 'a123456':
+		else:
+			print('密碼錯誤!還有', chance, '次機會')
+			typetimes += 1
+	elif text == password:
 		print('登入成功!')
 		break
